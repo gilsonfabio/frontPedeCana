a3797c6c79ac2ad52aaa92b5e664fbdd3287e5a4
Gilson Fábio <gilsonfabio@gmail.com>
2022-12-02 10:49:03 -0300

    First commit

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,17 @@
+export declare global {
+    namespace ReactNavigation {
+        interface RootParamList {
+            SignIn: undefined
+           Produtos: undefined;
+           Detalhes: {
+            proId: string;
+           }
+           CarShopping: {
+            carId: number;
+           }
+           FrmPagto: undefined;
+           CarCredit: undefined;
+           LocEntrega: undefined;
+        }
+    }
+}
